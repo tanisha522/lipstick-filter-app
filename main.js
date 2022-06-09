0e36fa36d31c94c399145c5869f07c066eba17fb
tanisha522 <104923162+tanisha522@users.noreply.github.com>
2022-06-09 21:52:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function preload()
+{
+
+}
+
+function setup()
+{
+    canvas = createCanvas(350,350);
+    canvas.center();
+}
+
+function draw()
+{
+
+}
+
+function take_snapshot()
+{
+    save('myfilterimage.png');
+}
